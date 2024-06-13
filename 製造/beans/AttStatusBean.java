02fa2bb468fa45a745a36b87c853460b57a434df
ToShinyu <user6@arc100.onmicrosoft.com>
2024-06-13 11:18:12 +0900

    update

--- 製造/beans/AttStatusBean.java
+++ 製造/beans/AttStatusBean.java
@@ -2,21 +2,31 @@
 //***　作成日時：2024/6/12
 package beans;
 
-import java.util.Date;
+import java.sql.Date;
 
 public class AttStatusBean {
+	//勤怠状況表テーブル主キー：勤怠状況表ID
 	private int att_status_id;
+	//外部キー：利用者ID
 	private int users_id;
+	//年月
 	private Date years;
+	//作成者
+	private int created_users_id;
+	//更新者
+	private int updated_users_id;
 
 	public AttStatusBean() {
 
 	}
 
-	public AttStatusBean(int att_status_id, int users_id, Date years) {
+	public AttStatusBean(int att_status_id, int users_id, Date years, int created_users_id, int updated_users_id) {
+		super();
 		this.att_status_id = att_status_id;
 		this.users_id = users_id;
 		this.years = years;
+		this.created_users_id = created_users_id;
+		this.updated_users_id = updated_users_id;
 	}
 
 	public int getUsers_id() {
@@ -38,6 +48,21 @@ public class AttStatusBean {
 	public int getAtt_status_id() {
 		return att_status_id;
 	}
-	
-	
+
+	public int getCreated_users_id() {
+		return created_users_id;
+	}
+
+	public void setCreated_users_id(int created_users_id) {
+		this.created_users_id = created_users_id;
+	}
+
+	public int getUpdated_users_id() {
+		return updated_users_id;
+	}
+
+	public void setUpdated_users_id(int updated_users_id) {
+		this.updated_users_id = updated_users_id;
+	}
+
 }
--- 製造/beans/CalendarBean.java
+++ 製造/beans/CalendarBean.java
@@ -5,20 +5,32 @@ package beans;
 import java.util.Date;
 
 public class CalendarBean {
+	//カレンダーテーブル主キー：カレンダーID
 	private int calendar_id;
+	//日付
 	private Date calendar_date;
+	//勤怠状況
 	private String work_status;
+	//備考
 	private String note;
+	//作成者
+	private int created_users_id;
+	//更新者
+	private int updated_users_id;
 	
-	public CalendarBean() {
+ 	public CalendarBean() {
 		
 	}
-	
-	public CalendarBean(int calendar_id, Date calendar_date, String work_status, String note) {
+
+	public CalendarBean(int calendar_id, Date calendar_date, String work_status, String note, int created_users_id,
+			int updated_users_id) {
+		super();
 		this.calendar_id = calendar_id;
 		this.calendar_date = calendar_date;
 		this.work_status = work_status;
 		this.note = note;
+		this.created_users_id = created_users_id;
+		this.updated_users_id = updated_users_id;
 	}
 
 	public Date getCalendar_date() {
@@ -49,5 +61,23 @@ public class CalendarBean {
 		return calendar_id;
 	}
 	
+
+	public int getCreated_users_id() {
+		return created_users_id;
+	}
 	
+
+	public void setCreated_users_id(int created_users_id) {
+		this.created_users_id = created_users_id;
+	}
+	
+
+	public int getUpdated_users_id() {
+		return updated_users_id;
+	}
+	
+
+	public void setUpdated_users_id(int updated_users_id) {
+		this.updated_users_id = updated_users_id;
+	}
 }
