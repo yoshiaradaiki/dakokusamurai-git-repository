711559263a51e28cd77f352d96b5df14bf434db9
ocssuzukitaisei <t_suzuki@ocsoff365.onmicrosoft.com>
2024-06-13 09:26:59 +0900

    Create StampDAO.java

---
+++ 製造/dao/StampDAO.java
@@ -0,0 +1,24 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+
+import beans.StampBean;
+
+public class StampDAO {
+    private final String JDBC_URL = "jdbc:h2:tcp://localhost/C:\\dakokuSamuraiDB\\dakokuSamuraiDB";
+    private final String DB_USER = "dakokuSamurai";
+    private final String DB_PASS = "dakokusamurai";
+    
+    //
+    //
+    // 
+    public StampBean insertStamp(user_id) {
+    }
+    
+
+
