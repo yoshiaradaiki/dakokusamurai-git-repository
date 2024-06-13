67902b6ba63d89d845fc8df9b9f8ddae9c9f658d
YokoyamaSayuri <user5@arc100.onmicrosoft.com>
2024-06-13 14:57:38 +0900

    更新
    StampRevBean作り直し
    AttDAO・StampDAOを更新

--- 製造/beans/StampRevBean.java
+++ 製造/beans/StampRevBean.java
@@ -1,93 +1,93 @@
 // 担当者：吉新
 // 作成日時：2024/06/12
+// 更新者：横山
 package beans;
 
-import java.sql.Timestamp;
+import java.time.LocalTime;
 
 public class StampRevBean {
 	// フィールド
-	private int request_id;
-	private int request_foreign_id;
-	private Timestamp date_and_time;
-	private String reason;
-	private int status;
-	private int content;
-	private String name;
-	private String boss_name;
+	private int stamp_rev_id;
+	private int stamp_id;
+	private LocalTime workIn_rev;
+	private LocalTime workOut_rev;
+	private LocalTime rest_time;
+	private int work_status;
+	private String note;
 	
 	// コンストラクタ
 	public StampRevBean() {}
-	
-	public StampRevBean(int request_id, int request_foreign_id, Timestamp date_and_time, String reason, int status,
-			int content, String name, String boss_name) {
-		this.request_id = request_id;
-		this.request_foreign_id = request_foreign_id;
-		this.date_and_time = date_and_time;
-		this.reason = reason;
-		this.status = status;
-		this.content = content;
-		this.name = name;
-		this.boss_name = boss_name;
+
+	public StampRevBean(int stamp_rev_id, int stamp_id, LocalTime workIn_rev, LocalTime workOut_rev,
+			LocalTime rest_time, int work_status, String note) {
+		this.stamp_rev_id = stamp_rev_id;
+		this.stamp_id = stamp_id;
+		this.workIn_rev = workIn_rev;
+		this.workOut_rev = workOut_rev;
+		this.rest_time = rest_time;
+		this.work_status = work_status;
+		this.note = note;
 	}
 
 	// ゲッターセッター
-	public int getRequest_id() {
-		return request_id;
+
+	public int getStamp_rev_id() {
+		return stamp_rev_id;
 	}
 
-	public void setRequest_id(int request_id) {
-		this.request_id = request_id;
+	public void setStamp_rev_id(int stamp_rev_id) {
+		this.stamp_rev_id = stamp_rev_id;
 	}
 
-	public int getRequest_foreign_id() {
-		return request_foreign_id;
+	public int getStamp_id() {
+		return stamp_id;
 	}
 
-	public void setRequest_foreign_id(int request_foreign_id) {
-		this.request_foreign_id = request_foreign_id;
+	public void setStamp_id(int stamp_id) {
+		this.stamp_id = stamp_id;
 	}
 
-	public Timestamp getDate_and_time() {
-		return date_and_time;
+	public LocalTime getWorkIn_rev() {
+		return workIn_rev;
 	}
 
-	public void setDate_and_time(Timestamp date_and_time) {
-		this.date_and_time = date_and_time;
+	public void setWorkIn_rev(LocalTime workIn_rev) {
+		this.workIn_rev = workIn_rev;
 	}
 
-	public String getReason() {
-		return reason;
+	public LocalTime getWorkOut_rev() {
+		return workOut_rev;
 	}
 
-	public void setReason(String reason) {
-		this.reason = reason;
+	public void setWorkOut_rev(LocalTime workOut_rev) {
+		this.workOut_rev = workOut_rev;
 	}
 
-	public int getStatus() {
-		return status;
+	public LocalTime getRest_time() {
+		return rest_time;
 	}
 
-	public void setStatus(int status) {
-		this.status = status;
+	public void setRest_time(LocalTime rest_time) {
+		this.rest_time = rest_time;
 	}
 
-	public int getContent() {
-		return content;
+	public int getWork_status() {
+		return work_status;
 	}
 
-	public void setContent(int content) {
-		this.content = content;
+	public void setWork_status(int work_status) {
+		this.work_status = work_status;
 	}
 
-	public String getName() {
-		return name;
+	public String getNote() {
+		return note;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setNote(String note) {
+		this.note = note;
 	}
-
-	public String getBoss_name() {
-		return boss_name;
-	}
+	
+	
+	
+	
 }
