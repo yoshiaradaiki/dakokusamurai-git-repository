9fe8b0287065e182c4ac16a6aa0bddef27b56783
ocssuzukitaisei <t_suzuki@ocsoff365.onmicrosoft.com>
2024-06-17 14:03:36 +0900

    a

---
+++ 製造/servlet/AttStatusController.java
@@ -0,0 +1,56 @@
+package servlet;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import beans.RequestListBean;
+import beans.StampBean;
+import beans.UsersBean;
+import logic.AttStatusLogic;
+
+@WebServlet("/AttStatusController")
+public class AttStatusController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+
+		// セッションから利用者IDを取得
+		HttpSession session = request.getSession();
+		UsersBean sessionUsersBean = (UsersBean) session.getAttribute("sessionUsersBean");
+		int users_id = sessionUsersBean.getUsers_id();
+
+		//現在の日付を取得
+		Date date = new Date();
+
+		// AttStatusLogic のインスタンスを生成
+		AttStatusLogic attStatusLogic = new AttStatusLogic();
+
+		//勤怠状況表の利用者取得
+		UsersBean usersBean = attStatusLogic.findMyAttStatusUsers1(users_id);
+
+		//勤怠状況表の表示
+		List<StampBean> stampBean = attStatusLogic.findMyAttStatusMonthStamp(users_id, date);
+
+		//差し戻し理由を一覧から取得
+		RequestListBean requestListBean = attStatusLogic.findMyAttStatusMonthRequest(users_id, date);
+
+		//JSPから取得するためにセットする
+		request.setAttribute("usersBean", usersBean);//利用者ID
+		request.setAttribute("stampBean", stampBean);//勤怠状況表
+		request.setAttribute("requestListBean", requestListBean); //理由
+
+		//"attendanceStatus.jsp"へ転送する
+		request.getRequestDispatcher("WEB-INF/jsp/attendanceStatus.jsp").forward(request, response);
+	}
+}
--- 製造/servlet/WorkOutController.java
+++ 製造/servlet/WorkOutController.java
@@ -1,7 +1,8 @@
 package servlet;
 
 import java.io.IOException;
-import java.util.Date; // Dateクラスをインポート
+import java.time.LocalTime;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,9 +11,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import beans.UsersBean;
 import logic.EmpLogic;
 
-@WebServlet("/WorkOutController")
+@WebServlet("/WolkOutController")
 public class WorkOutController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -26,11 +28,26 @@ public class WorkOutController extends HttpServlet {
 
 		// セッションから利用者IDを取得
 		HttpSession session = request.getSession();
-		int users_id = (int) session.getAttribute("users_id");
+		UsersBean sessionUsersBean = (UsersBean) session.getAttribute("sessionUsersBean");
+		int users_id = sessionUsersBean.getUsers_id();
 
 		// 現在時刻を取得
 		Date date = new Date();
 
-	}
+		// 現在時刻からLocalTimeを取得
+		LocalTime workOut_raw = LocalTime.now();
 
+		//登録処理
+		EmpLogic empLogic = new EmpLogic();
+		boolean result = empLogic.updateStamp(users_id, date, workOut_raw);
+
+		if (result) {
+			//登録成功（社員画面リダイレクト）
+			request.getRequestDispatcher("userMain.jsp").forward(request, response);
+		} else {
+			//登録失敗（エラー表示＆社員画面リダイレクト）
+			request.setAttribute("errorMsg", "出勤時刻の登録に失敗しました。");
+			request.getRequestDispatcher("WEB-INF/jsp/userMain.jsp").forward(request, response);
+		}
+	}
 }
