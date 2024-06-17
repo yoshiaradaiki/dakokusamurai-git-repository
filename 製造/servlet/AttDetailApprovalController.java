e62fde0d34e8bcc6feabe2d56b2bec7b2e6b0833
NagaeRikuya <user1@arc100.onmicrosoft.com>
2024-06-17 14:28:11 +0900

    Update AttDetailApprovalController.java

--- 製造/servlet/AttDetailApprovalController.java
+++ 製造/servlet/AttDetailApprovalController.java
@@ -1,5 +1,5 @@
 //担当:長江
-//2024/06/14
+//更新日：2024/06/17
 //勤怠状況詳細の承認ボタン
 
 package servlet;
@@ -28,33 +28,36 @@ public class AttDetailApprovalController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		request.setCharacterEncoding("UTF-8");
+//		　　セッションを取得
+		HttpSession session = request.getSession();
+		
+//		自分の申請一覧：取得した情報をUserBeanに渡す
+		UsersBean sessionUsersBean = (UsersBean)session.getAttribute("sessionUsersBean");
+//      セッションから利用者IDを取得
+		int users_id = sessionUsersBean.getUsers_id();
 		
 //        部下の打刻修正申請を承認する
 		
 		AttDetailLogic attDetailLogic = new AttDetailLogic();
 		
-		int stamp_rev_req_id =Integer.parseInt( request.getParameter("stamp_rev_req_id"));
+//		hiddenから取得
+		int stamp_rev_req_id =Integer.parseInt( request.getParameter("stamp_rev_req_id"));//打刻修正申請IDを取得
 		int status = 2; //承認済み
 		String reason = "";  //承認時は理由は不要
 		
-		attDetailLogic.updateStampRevReq(stamp_rev_req_id, status, reason);
+//		打刻修正申請更新を実行し、打刻修正申請ID,ステータス、理由の結果を取得
+		attDetailLogic.updateStampRevReq(stamp_rev_req_id, status, reason,users_id);
 		
-        HttpSession session = request.getSession();
-        
-//		自分の申請一覧
-		UsersBean sessionUsersBean = (UsersBean)session.getAttribute("sessionUsersBean");
-		int users_id = sessionUsersBean.getUsers_id();
-		List<RequestListBean> myRequestListBeans = attDetailLogic.findMyRequest(users_id);
+//　　　申請一覧を取得
+		List<RequestListBean> myRequestListBean = attDetailLogic.findMyRequest(users_id);
+		List<RequestListBean> subRequestListBean = attDetailLogic.findMySubRequest(users_id);
 		
-		request.setAttribute("myRequestListBeans", myRequestListBeans);
-
-//		部下の申請一覧
-		UsersBean sessionUsersBean = (UsersBean)session.getAttribute("sessionUsersBean");
-		int users_id = sessionUsersBean.getUsers_id();
-		List<RequestListBean> mySubRequestListBeans = attDetailLogic.findMySubRequest(users_id);
 		
-		request.setAttribute("mySubRequestListBeans", mySubRequestListBeans);
+//　　　遷移先画面である申請一覧画面へ値を渡す					
+		request.setAttribute("myRequestListBean", myRequestListBean);
+		request.setAttribute("subRequestListBean", subRequestListBean);	
 		
+//		JSPからサーブレットへ転送		
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/requestList.jsp");
 		dispatcher.forward(request, response);
 
