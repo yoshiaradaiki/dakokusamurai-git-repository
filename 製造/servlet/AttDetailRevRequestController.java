b36efddf41f01daa3b9c793dda3d8e983722960c
NagaeRikuya <user1@arc100.onmicrosoft.com>
2024-06-17 14:28:29 +0900

    Update AttDetailRevRequestController.java

--- 製造/servlet/AttDetailRevRequestController.java
+++ 製造/servlet/AttDetailRevRequestController.java
@@ -1,11 +1,12 @@
 //担当:長江
-//2024/06/14
+//更新日：2024/06/17
 //勤怠状況詳細画面の変更申請ボタン
 package servlet;
 
 import java.io.IOException;
 import java.time.LocalTime;
 import java.util.Date;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -15,15 +16,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import beans.RequestListBean;
 import beans.StampBean;
+import beans.StampRevBean;
 import beans.UsersBean;
 import logic.AttDetailLogic;
-
 
 @WebServlet("/AttDetailRevRequestController")
 public class AttDetailRevRequestController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    
+
 	// 文字列型をLocalTime型に変換
 	private LocalTime stringToLocalTime(String s) {
 		if (s != null) {
@@ -32,63 +34,94 @@ public class AttDetailRevRequestController extends HttpServlet {
 			return null;
 		}
 	}
-		
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
-		
+//     セッションを取得
 		HttpSession session = request.getSession();
+//　　　取得した情報をUserBeanに渡す
+		UsersBean sessionUsersBean = (UsersBean) session.getAttribute("sessionUsersBean");
+//      申請日を取得
+		Date requestDate = (Date) request.getAttribute("requestDate");
+
+		StampBean stampBean = new StampBean();//打刻データ
 		
-		UsersBean sessionUsersBean = (UsersBean)session.getAttribute("sessionUsersBean");
+		int users_id = sessionUsersBean.getUsers_id();//利用者IDを取得
 		
-		Date requestDate = (Date)request.getAttribute("requestDate");
+		stampBean.setUsers_id(users_id);//利用者IDを取得して保持
 		
-		StampBean  stampBean= new StampBean();
-		stampBean.setUsers_id(sessionUsersBean.getUsers_id());
-		stampBean.setStamp_date(requestDate);
+		stampBean.setStamp_date(requestDate);//申請日を保持
+
+		//StampRevBean stampRevBean = new StampRevBean();
+		int stamp_id = Integer.parseInt(request.getParameter("stamp_id"));//打刻IDを取得
+
+		int work_status = Integer.parseInt(request.getParameter("work_status"));//勤怠状況
+
+		LocalTime workIn_rev = stringToLocalTime(request.getParameter("workIn_rev"));//修正出勤時刻
+
+		LocalTime workOut_rev = stringToLocalTime(request.getParameter("workOut_rev"));//修正退勤時刻
+
+		//		LocalTime date = stringToLocalTime(request.getParameter("workOut_rev"));
+
+		LocalTime rest_time = stringToLocalTime(request.getParameter("rest_time"));//休憩
+
+		String note = request.getParameter("note");//備考
 		
-		int work_status = Integer.parseInt(request.getParameter("work_status"));
+		String name = request.getParameter("name");//申請者
 		
-		LocalTime workIn_rev = stringToLocalTime(request.getParameter("workIn_rev"));
+		String reason = request.getParameter("reason");//差し戻し理由
 		
-		LocalTime workOut_rev = stringToLocalTime(request.getParameter("workOut_rev"));
+//		int content = 0;//内容　変更申請
+
+		int status = 1; //ステータス　承認待ち
+
+		//	打刻情報チェック
+		AttDetailLogic attDetailLogic = new AttDetailLogic();
+		//打刻チェックを行い、利用者ID,申請日,打刻情報の結果を取得
+		attDetailLogic.findStampCheck(sessionUsersBean.getUsers_id(), requestDate);
+
+		//　打刻修正テーブルに登録
+		StampRevBean stampRevBean = new StampRevBean();
 		
-		LocalTime date = stringToLocalTime(request.getParameter("workOut_rev"));
+//		打刻修正データに値を設定
+		stampRevBean.setStamp_id(stamp_id);
+		stampRevBean.setWorkIn_rev(workIn_rev);
+		stampRevBean.setWorkOut_rev(workOut_rev);
+		stampRevBean.setRest_time(rest_time);
+		stampRevBean.setWork_status(work_status);
+		stampRevBean.setNote(note);
 		
-		LocalTime rest_time = stringToLocalTime(request.getParameter("rest_time"));
-		
-		
-		String note = request.getParameter("note");
-		
-		int status = 1;
-		
-		int content= request.getParameter("content"); 
-				
-		
-//		打刻情報チェック
-		AttDetailLogic attDetailLogic = new AttDetailLogic();	
-		attDetailLogic.findStampCheck(sessionUsersBean.getUsers_id(),requestDate,stampBean);
-		
-		
-		
-//　　　打刻修正テーブルに登録
-		boolean stampRevBean = new AttDetailLogic().insertStampRev(work_status,workIn_rev,workOut_rev,rest_time,note); 
-		
-		
-		
-		
-//　　　打刻修正申請を提出　
-		boolean requestListBean = new AttDetailLogic().insertStampRevReq(date_and_time,status,content);
+       //  打刻修正追加を行い、打刻ID、修正出勤、修正退勤、休憩、勤怠状況、備考の結果を取得		
+		attDetailLogic.insertStampRev(stampRevBean);
 		
 		
 		
-					
-		request.setAttribute(requestListBean,requestListBean);
+
+		//　打刻修正申請テーブルに登録
+		 RequestListBean requestListBean= new RequestListBean ();
+		 requestListBean.setStamp_rev_id(attDetailLogic.findStampRevId(stamp_id));
+		 requestListBean.setStatus(1);
+		 requestListBean.setCreated_users_id(users_id);
+		 requestListBean.setUpdated_users_id(users_id);
+		 
+		 
+		 //打刻修正申請追加を行い、理由、ステータス、申請者の結果を取得
+		attDetailLogic.insertStampRevReq(requestListBean);
 		
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/attendanceStatusDetail.jsp");
+		//申請一覧を取得
+		List<RequestListBean> myRequestListBean = attDetailLogic.findMyRequest(users_id);
+		List<RequestListBean> subRequestListBean = attDetailLogic.findMySubRequest(users_id);
+		
+		
+		//　遷移先画面である申請一覧画面へ値を渡す					
+		request.setAttribute("myRequestListBean", myRequestListBean);
+		request.setAttribute("subRequestListBean", subRequestListBean);
+
+		//	JSPからサーブレットへ転送
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/RequestList.jsp");
 		dispatcher.forward(request, response);
 
 	}
-
-	
 
 }
