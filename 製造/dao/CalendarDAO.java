c8f1000e8d75e3a6a42ab10d4d18a98420f3cf17
yoshiaradaiki <d_yoshiara@ocsoff365.onmicrosoft.com>
2024-06-24 16:49:52 +0900

    Update CalendarDAO.java

--- 製造/dao/CalendarDAO.java
+++ 製造/dao/CalendarDAO.java
@@ -357,4 +357,126 @@ public class CalendarDAO {
 		}
 		return stampBean;
 	}
+	
+	// 1日分取得するメソッド
+	public StampBean findMyAttStatusDetailStampRequest(int stamp_rev_id) {
+		
+		Connection con = null;
+		PreparedStatement psmt = null;
+		ResultSet rs = null;
+		// SQL文
+		String sql = ""
+				+ "SELECT c.calendar_date,\r\n"
+				+ "		s.workIn_raw,\r\n"
+				+ "		s.workOut_raw,\r\n"
+				+ "		COALESCE(sr.workIn_rev, s.workIn_re) AS workIn_re,\r\n"
+				+ "		COALESCE(sr.workOut_rev, s.workOut_re) AS workOut_re,\r\n"
+				+ "		COALESCE(sr.work_status, c.work_status) AS work_status,\r\n"
+				+ "		sr.rest_time,\r\n"
+				+ "		COALESCE(sr.note, c.note) AS note,\r\n"
+				+ "		s.stamp_id\r\n"
+				+ "	FROM calendar c\r\n"
+				+ "	LEFT JOIN STAMP s ON c.calendar_date = s.stamp_date\r\n"
+				+ "	LEFT JOIN STAMP_REVISION sr ON s.stamp_id = sr.stamp_id\r\n"
+				+ "	LEFT JOIN STAMP_CORRECT_REQ scr ON sr.stamp_rev_id = scr.stamp_rev_id\r\n"
+				+ "	WHERE sr.stamp_rev_id = ?;";
+		
+		StampBean stampBean = null;
+		
+		try {
+			// DB接続
+			con = ConnectionDB();
+						
+			psmt = con.prepareStatement(sql);
+			psmt.setInt(1, stamp_rev_id);
+			
+			// SQLの結果を取得
+			rs = psmt.executeQuery();
+			// １行ずつ取得
+			while (rs.next()) {
+				// StampBeanのインスタンス化
+				stampBean = new StampBean();
+				// 日付の取得(yyyy-mm-dd)
+				stampBean.setStamp_date(rs.getDate("calendar_date"));
+				// 出勤時刻の取得(hh:mm)
+				stampBean.setWorkIn_raw(timeNullCheck(rs.getTime("workIn_raw")));
+				// 退勤時刻の取得(hh:mm)
+				stampBean.setWorkOut_raw(timeNullCheck(rs.getTime("workOut_raw")));
+				// 補正出勤時刻の取得(hh:mm)
+				stampBean.setWorkIn_re(timeNullCheck(rs.getTime("workIn_re")));
+				// 補正退勤時刻の取得(hh:mm)
+				stampBean.setWorkOut_re(timeNullCheck(rs.getTime("workOut_re")));
+				// 勤怠状況の取得(int)
+				stampBean.setWork_status(rs.getInt("work_status"));
+				// 休憩時間の取得(hh:mm)
+				stampBean.setRest_time(timeNullCheck(rs.getTime("rest_time")));
+				// 出勤しているが休憩時間が取得できない場合はデフォルトで1:00を入れる
+				if (stampBean.getRest_time() == null && stampBean.getWorkIn_raw() != null) {
+					LocalTime localTime = LocalTime.of(1, 0);
+					stampBean.setRest_time(localTime);
+				}
+				// 日付から曜日の取得(int)
+				if (stampBean.getStamp_date() != null) {
+					Calendar calendarWeek = Calendar.getInstance();
+					calendarWeek.setTime(stampBean.getStamp_date());
+					// 曜日の取得と挿入
+					stampBean.setWeek(calendarWeek.get(Calendar.DAY_OF_WEEK));
+				}
+				// 補正出退勤時刻と休憩時間をもとに実労働時間を取得(hh:mm)
+				if (stampBean.getWorkIn_re() != null && 
+					stampBean.getWorkOut_re() != null && 
+					stampBean.getRest_time() != null) {
+					// 時刻を分にする
+					int wi = stampBean.getWorkIn_re().getHour() * 60 + stampBean.getWorkIn_re().getMinute();
+					int wo = stampBean.getWorkOut_re().getHour() * 60 + stampBean.getWorkOut_re().getMinute();
+					int rt = stampBean.getRest_time().getHour() * 60 + stampBean.getRest_time().getMinute();
+					// 差を計算
+					int rwt = wo - wi - rt;
+					if (rwt < 0) {
+						rwt = 0;
+					}
+					// 時刻に戻す
+					int rwtH = rwt / 60;
+					int rwtM = rwt % 60;
+					LocalTime localTime = LocalTime.of(rwtH, rwtM);
+					stampBean.setReal_work_time(localTime);
+				}
+				// 備考の取得(String)
+				stampBean.setNote(rs.getString("note"));
+				// 打刻ID(int)
+				stampBean.setStamp_id(rs.getInt("stamp_id"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("DBアクセスエラー");
+			System.out.println(e.getMessage());
+		} catch (NullPointerException e) {
+			e.printStackTrace();
+			System.out.println("ぬるぽ");
+			System.out.println(e.getMessage());
+		} finally {
+			if(rs != null){
+		        try{
+		            rs.close();
+		        } catch (SQLException e) {
+		        	System.out.println(e.getMessage());
+		        }
+		    }
+		        if(psmt != null){
+		        try{
+		        	psmt.close();
+		        } catch (SQLException e) {
+		        	System.out.println(e.getMessage());
+		        }
+		    }
+		        if(con != null){
+		        try{
+		            con.close();
+		        } catch (SQLException e) {
+		        	System.out.println(e.getMessage());
+		        }
+		    }
+		}
+		return stampBean;
+	}
 }
